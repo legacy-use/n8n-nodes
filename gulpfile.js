64cbc08b4cc88fadbbb69014af0e7a3206f45c27
Thilo Reintjes <thilo.reintjes@gmail.com>
2025-08-22 15:06:13 +0200

    Update version to 0.1.4 and modify gulpfile.js to adjust source and destination paths for icons and credentials, enhancing the build process for LegacyUse assets.

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const { task, src, dest } = require('gulp');
 task('build:icons', copyIcons);
 
 function copyIcons() {
-	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
-	const nodeDestination = path.resolve('dist', 'nodes');
+	const nodeSource = path.resolve('nodes', 'LegacyUse', '*.{png,svg}');
+	const nodeDestination = path.resolve('dist', 'nodes', 'LegacyUse');
 
 	src(nodeSource).pipe(dest(nodeDestination));
 
-	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
+	const credSource = path.resolve('credentials', 'LegacyUseApi*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
 
 	return src(credSource).pipe(dest(credDestination));
